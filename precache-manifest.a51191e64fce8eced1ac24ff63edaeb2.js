fc235f1f2a8059627a9c07d6c1b949c1f70c3f49
Aiden Oliver <31047390+AidenOliver@users.noreply.github.com>
2020-11-22 12:14:54 -0700

    Updates

---
+++ precache-manifest.a51191e64fce8eced1ac24ff63edaeb2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d2c1511345aeae46cdf4d0a38226202",
+    "url": "/recipe-app/index.html"
+  },
+  {
+    "revision": "44f9737897490e26c6df",
+    "url": "/recipe-app/static/css/main.6adcf274.chunk.css"
+  },
+  {
+    "revision": "c64fbda19e0d5ecd2195",
+    "url": "/recipe-app/static/js/2.01553eef.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/recipe-app/static/js/2.01553eef.chunk.js.LICENSE"
+  },
+  {
+    "revision": "44f9737897490e26c6df",
+    "url": "/recipe-app/static/js/main.912b0a50.chunk.js"
+  },
+  {
+    "revision": "4dc816ca4199b33625da",
+    "url": "/recipe-app/static/js/runtime-main.ac0cb26e.js"
+  }
+]);
